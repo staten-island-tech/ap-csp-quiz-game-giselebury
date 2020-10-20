5d6f44475c28435025ba3617dea5036e3fb1f7cc
giselebury <54081443+giselebury@users.noreply.github.com>
2020-10-19 22:54:08 -0400

    js

--- src/js/DOM.js
+++ src/js/DOM.js
@@ -1,5 +1,6 @@
 const DOMSelectors = {
     quiz: document.querySelector(".quiz"),
+    start: document.querySelector(".quiz-start"),
 }
 
 export {DOMSelectors};
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,16 @@
 import {DOMSelectors} from "./DOM";
 import {quiz} from "./questions";
 
+const init = function() {
+    DOMSelectors.start.addEventListener("click", function(e){
+        DOMSelectors.start.innerHTML = "";
+        DOMSelectors.quiz.insertAdjacentElement("afterbegin",
+        `<div class="quiz-score">
+        <h2>Score</h2>
+        <p>0</p>
+    </div>`)
+    });
+}
 
+init();
 
