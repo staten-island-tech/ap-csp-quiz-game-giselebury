22084bc19fe9b5f995297a2efc2862b6ffcac579
giselebury <54081443+giselebury@users.noreply.github.com>
2020-10-20 23:51:46 -0400

    displayed quiz

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,32 @@
 import {DOMSelectors} from "./DOM";
 import {quiz} from "./questions";
 
-const init = function() {
+const start = function() {
     DOMSelectors.start.addEventListener("click", function(e){
-        DOMSelectors.start.innerHTML = "";
-        DOMSelectors.quiz.insertAdjacentElement("afterbegin",
-        `<div class="quiz-score">
-        <h2>Score</h2>
-        <p>0</p>
-    </div>`)
+        
+        
     });
 }
 
-init();
 
+const init = function(){
+    DOMSelectors.start.addEventListener("click", function(e){
+       DOMSelectors.start.innerHTML = ""; 
+        quiz.forEach((item) =>
+        DOMSelectors.start.insertAdjacentHTML("beforeend",
+        `<li>${item.question}</li>
+        <li class = "quiz-img"><img src="${item.img}" alt=""></li>
+        <button class="answer-btn">${item.answers[0]}</button>
+        <button class="answer-btn">${item.answers[1]}</button>
+        <button class="answer-btn">${item.answers[2]}</button>
+        <button class="answer-btn">${item.answers[3]}</button>
+         `
+
+        ));
+    })
+    
+        
+    
+}
+
+init();
