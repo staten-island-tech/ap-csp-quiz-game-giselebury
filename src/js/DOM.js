093f752c7cbe8782380bc93043881dc712113697
giselebury <54081443+giselebury@users.noreply.github.com>
2020-10-22 12:13:58 -0400

    fixed checking for questions

--- src/js/DOM.js
+++ src/js/DOM.js
@@ -1,10 +1,6 @@
 const DOMSelectors = {
     quiz: document.querySelector(".quiz"),
     start: document.querySelector(".quiz-start"),
-    one:document.getElementById("answer-1"),
-    two:document.getElementById("answer-2"),
-    three:document.getElementById("answer-3"),
-    four:document.getElementById("answer-4"),
     
 }
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,86 +1,73 @@
 import {DOMSelectors} from "./DOM";
 import {quiz} from "./questions";
 
+const init = function () {
+    let started = false;
 
+    function loadHtml(){
+        DOMSelectors.start.innerHTML = "";
+        quiz.forEach((item) =>
+        DOMSelectors.start.insertAdjacentElement("beforeend",
+        `<div class = "quiz" id = ${item.number}>
+        <li class = "quiz-question">${item.question}</li>
+        <button class="answer-btn" id = "answer-1">${item.answers[0]}</button>
+        <button class="answer-btn" id = "answer-2">${item.answers[1]}</button>
+        <button class="answer-btn" id = "answer-3">${item.answers[2]}</button>            
+        <button class="answer-btn" id = "answer-4">${item.answers[3]}</button>
+        </div>
+        `
+        ));
 
+        document.querySelectorAll(".answer-btn").forEach((btn) => {
+            btn.addEventListener("click", (e) => {
+                const quizContainer = e.target.parentElement;
+                const btn = e.target;
+                checkAnswer(quizContainer,btn);
+            })
+        })
+        //DOMSelectors.start.removeEventListener("click", loadHtml);
+    };
 
+    DOMSelectors.start.addEventListener("click", loadHtml);
+}
 
-const init = function(){
-    DOMSelectors.start.addEventListener("click", function(e){
-        DOMSelectors.start.innerHTML = ""; 
-            quiz.forEach((item) =>
-            DOMSelectors.start.insertAdjacentHTML("beforeend",
-            `<div class = "quiz">
-            <li class = "quiz-question">${item.question}</li>
-            <button class="answer-btn" id = "answer-1">${item.answers[0]}</button>
-            <button class="answer-btn" id = "answer-2">${item.answers[1]}</button>
-            <button class="answer-btn" id = "answer-3">${item.answers[2]}</button>
-            <button class="answer-btn" id = "answer-4">${item.answers[3]}</button>
-            </div>
-            
-             `
-        
-    ));
-})
-}       
-
-
-
-const correctAnswer = function(){
+const correctAnswer = function(id) {
     let answer = 0;
-    document.addEventListener("click", function(e){
-      if(e.target.id === "answer-1" ){
-          answer = 0
-      }  
-    })
-    document.addEventListener("click", function(e){
-        if(e.target.id === "answer-2" ){
-            answer = 1
-        }  
-      })
-    
-    document.addEventListener("click", function(e){
-        if(e.target.id === "answer-3" ){
-            answer = 2
-        }  
-      })
-    
-    document.addEventListener("click", function(e){
-        if(e.target.id === "answer-4" ){
-            answer = 3
-        }  
-      })
-    
-    
+    switch (id) {
+        case "answer-1":
+            answer = 0;
+            break;
+        case "answer-2":
+            answer = 1;
+            break;
+        case "answer-3":
+            answer = 2;
+            break;
+        case "answer-4":
+            answer = 3;
+            break;  
+    }
+    return answer
 }
 
-const checkAnswer = function(){
-    correctAnswer()
-    if(answer === quiz.correct){
-        DOMSelectors.one.className = "btn-correct"
+function checkAnswer(quizContainer, btn){
+    console.log(quizContainer);
+    const answer = correctAnswer(btn.id);
+    const questionindex = quizContainer.id - 1;
+
+    if (answer === quiz[questionindex].correct) {
+        console.log('yes');
+        btn.classList.add("btn-correct");
     } else {
-        DOMSelectors.one.className = "btn-incorrect"
-    }
-    if(answer === quiz.correct){
-        DOMSelectors.two.className = "btn-correct"
-    } else {
-        DOMSelectors.two.className = "btn-incorrect"
-    }
-    if(answer === quiz.correct){
-        DOMSelectors.three.className = "btn-correct"
-    } else {
-        DOMSelectors.three.className = "btn-incorrect"
-    }
-    if(answer === quiz.correct){
-        DOMSelectors.four.className = "btn-correct"
-    } else {
-        DOMSelectors.four.className = "btn-incorrect"
+        console.log('no');
+        btn.classList.add("btn-incorrect");
     }
 }
-    
-    
- 
+
+
+
 
 init();
+loadHtml();
 correctAnswer();
-checkAnswer();
+checkAnswer(); 
